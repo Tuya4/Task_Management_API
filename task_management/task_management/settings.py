6d84fb96cbed5ef2910a5e74f8840cb7e19ea00d
Tuya4 <mohansang04@gmail.com>
2024-10-15 17:20:29 +0300

    update

--- task_management/task_management/settings.py
+++ task_management/task_management/settings.py
@@ -91,9 +91,10 @@ WSGI_APPLICATION = 'task_management.wsgi.application'
 #         'PORT': '5432',
 #     }
 # }
-DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
-}
+# DATABASES = {
+#     'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+# }
+DATABASES = {'default' : dj_database_url.config(conn_max_age=600, ssl_require=True)}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
