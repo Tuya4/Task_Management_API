f5d08b5b552ebac899cb448a7ba45cc675fbc381
Tuya4 <mohansang04@gmail.com>
2024-10-18 17:57:44 +0300

    update

--- task_management/tasks/serializers.py
+++ task_management/tasks/serializers.py
@@ -17,8 +17,8 @@ class TaskSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Task
-        fields = ['id', 'title', 'description', 'due_date', 'priority', 'status', 'completed_at', 'user', 'category', 'category_id', 'recurrence', 'next_due_date']
-        read_only_fields = ['completed_at', 'user', 'next_due_date']
+        fields = ['id', 'title', 'description', 'due_date', 'priority', 'status', 'completed_at', 'category', 'category_id', 'recurrence', 'next_due_date']
+        read_only_fields = ['completed_at', 'next_due_date']
     
     def validate(self, data):
         # Prevent editing of completed tasks unless reverting to Pending
